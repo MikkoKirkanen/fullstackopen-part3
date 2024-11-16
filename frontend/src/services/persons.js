7906bccb03d6575ee2b86895dda320cbb13b10d0
Mikko Kirkanen <mikko.kirkanen@gmail.com>
2024-11-16 13:00:19 +0200

    Update baseUrl name and path

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
-// const dbUrl = "http://localhost:3001/api/persons";
-const dbUrl = "https://fullstackopen-part3-nv2c.onrender.com/api/persons";
+// const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
-  return axios.get(dbUrl).then(response => response.data);
+  return axios.get(baseUrl).then(response => response.data);
 };
 
 const create = (newObject) => {
-  return axios.post(dbUrl, newObject).then(response => response.data);
+  return axios.post(baseUrl, newObject).then(response => response.data);
 };
 
 const remove = (id) => {
-  return axios.delete(`${dbUrl}/${id}`).then(response => response.data);
+  return axios.delete(`${baseUrl}/${id}`).then(response => response.data);
 };
 
 const update = (updateObject) => {
-  return axios.put(dbUrl, updateObject).then(response => response.data);
+  return axios.put(baseUrl, updateObject).then(response => response.data);
 };
 
 export default {
