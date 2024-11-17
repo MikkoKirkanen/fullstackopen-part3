b99ff52c2c19718ad3a37655a81b83db63a2de59
Mikko Kirkanen <mikko.kirkanen@gmail.com>
2024-11-17 19:46:31 +0200

    Fixed backend url to frontend

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   console.log('Connecting to MongoDB');
   return await mongoose
-    .connect(process.env.MONGODB_URI || '/api/persons')
+    .connect(process.env.MONGODB_URI)
     .then(() => {
       console.log('Connected to MongoDB');
     })
