1f10c7410202f7f0274c71b4ddb8e2cbbb62cee9
Mikko Kirkanen <mikko.kirkanen@gmail.com>
2024-11-19 22:33:20 +0200

    Update eslint config

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,12 +1,19 @@
 import globals from 'globals'
-import js from '@eslint/js'
 import stylisticJs from '@stylistic/eslint-plugin-js'
+import js from '@eslint/js'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
-    languageOptions: { globals: globals.node },
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+    },
     plugins: {
       '@stylistic/js': stylisticJs,
     },
@@ -15,7 +22,14 @@ export default [
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     },
-    ignores: ['dist/**'],
+  },
+  {
+    ignores: ['dist/**', 'build/**'],
   },
 ]
